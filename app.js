e2e4b60109ff72236779b17cfbbca7fccb0618ca
khubaib shah <160419480+Khubaib-shah@users.noreply.github.com>
2024-06-01 12:05:50 +0500

    complete todo-list

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
-let inputField = document.getElementById("InputField");
-let addTodoButton = document.getElementById("addToDo");
-let todoConainer = document.getElementById("todoConainer");
+function addTodo() {
+  var todoParent = document.getElementById("todoParent");
+  var todoInput = document.getElementById("todoInput");
+  if (todoInput.value.length < 3) {
+    alert("Enter correct todo value");
+    return;
+  }
+  var li = document.createElement("li");
+  li.innerHTML = todoInput.value;
+  var editBtn = document.createElement("button");
+  editBtn.setAttribute("onClick", "editTodo(this)");
+  editBtn.innerHTML = "Edit";
+  var delBtn = document.createElement("button");
+  delBtn.innerHTML = "Delete";
+  delBtn.setAttribute("onClick", "delTodo(this)");
+  li.appendChild(editBtn);
+  li.appendChild(delBtn);
+  todoParent.appendChild(li);
+  todoInput.value = "";
+}
+function delAll() {
+  var todoParent = document.getElementById("todoParent");
+  todoParent.innerHTML = "";
+}
 
-addTodoButton.addEventListener("click", function () {
-  var paragraph = document.createElement("p");
-  paragraph.classList.add("paragraph-styling");
-  paragraph.innerText = inputField.value;
-  todoConainer.appendChild("paragraph");
-  paragraph.addEventListener("click", function () {
-    paragraph.style.textDecoration = "line-through";
-  });
-});
+function delTodo(ele) {
+  console.log("delTodo", ele.parentNode);
+
+  ele.parentNode.remove();
+}
+
+function editTodo(ele) {
+  var editVal = prompt("Enter value", ele.previousSibling.nodeValue)
+  console.log(editVal)
+  ele.previousSibling.nodeValue = editVal
+
+}
