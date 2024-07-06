75ba32990820f529dc28a334c96fb88d204ebe89
khubaib shah <160419480+Khubaib-shah@users.noreply.github.com>
2024-07-07 02:31:32 +0500

    complete todo with new design
    CSS
    modified color pallete used shadow to the text added hover effects to the button

--- app.js
+++ app.js
@@ -5,18 +5,19 @@ function addTodo() {
     alert("Enter correct todo value");
     return;
   }
+  var todoText = capitalizeFirstLetter(todoInput.value);
   var li = document.createElement("li");
-  li.innerHTML = todoInput.value;
+  li.innerHTML = todoText;
   var editBtn = document.createElement("button");
   editBtn.setAttribute("onClick", "editTodo(this)");
-  editBtn.style.color = "green" 
+  editBtn.style.color = "green";
   editBtn.innerHTML = "Edit";
   var compBtn = document.createElement("button");
   compBtn.setAttribute("onClick", "comTodo(this)");
-  compBtn.style.color = "orange" 
+  compBtn.style.color = "orange";
   compBtn.innerHTML = "Complete";
   var delBtn = document.createElement("button");
-  delBtn.style.color = "red"
+  delBtn.style.color = "red";
   delBtn.innerHTML = "Delete";
   delBtn.setAttribute("onClick", "delTodo(this)");
   li.appendChild(editBtn);
@@ -24,6 +25,9 @@ function addTodo() {
   li.appendChild(delBtn);
   todoParent.appendChild(li);
   todoInput.value = "";
+}
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 function delAll() {
   var todoParent = document.getElementById("todoParent");
@@ -37,7 +41,7 @@ function editTodo(ele) {
 }
 function comTodo(completed) {
   completed.innerHTML = "Completed";
-  completed.style.color = "blue" 
+  completed.style.color = "blue";
 }
 function delTodo(ele) {
   ele.parentNode.remove();
